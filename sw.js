6aed142891a7f5bebb043236e697e0ca4305c464
jiangchengjun <jiangchengjun@joyy.com>
2026-05-11 10:30:49 +0800

    feat: add PWA support

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'jianpu-editor-v1';
+const PRECACHE = ['./', './index.html'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((k) => k !== CACHE_NAME)
+          .map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+
+  const url = new URL(req.url);
+  const isSameOrigin = url.origin === self.location.origin;
+
+  e.respondWith(
+    caches.match(req).then((cached) => {
+      // 优先返回缓存，同时后台更新
+      const update = fetch(req)
+        .then((networkRes) => {
+          if (networkRes && networkRes.ok && isSameOrigin) {
+            const clone = networkRes.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(req, clone));
+          }
+          return networkRes;
+        })
+        .catch(() => cached);
+
+      return cached || update;
+    })
+  );
+});
